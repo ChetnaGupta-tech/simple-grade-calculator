c9e5393ee3df050a00484ef6265288eee1c2ef30
Chetna Guptaa <74051849+ChetnaGupta-tech@users.noreply.github.com>
2026-03-23 19:22:52 +0530

    updated the function to calculate total, average and grade

---
+++ script.js
@@ -0,0 +1,24 @@
+let calculateResult = document.querySelector("#calculateResult");
+
+calculateResult.addEventListener("click", function(){
+    let subject1 = Number(document.getElementById("subject1").value);
+    let subject2 = Number(document.getElementById("subject2").value);
+    let subject3 = Number(document.getElementById("subject3").value);
+    let subject4 = Number(document.getElementById("subject4").value);
+
+    let total = subject1+subject2+subject3+subject4;
+    let average =total/4;
+    let grade;
+
+    if(average>=90)
+        grade = "A";
+    else if(average >=75)
+        grade = "B";
+    else if(average>=50)
+        grade = "C";
+    else 
+        grade = "D";
+
+    document.querySelector("h3").innerText= `Total = ${total} | Average = ${average} | Grade = ${grade}`
+    
+});
